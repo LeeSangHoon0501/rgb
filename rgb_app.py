25c7faf4e4c007880636319701f9f56e79074129
idealhoon0501 <144407867+LeeSangHoon0501@users.noreply.github.com>
2023-12-07 22:23:21 +0900

    Add files via upload

---
+++ rgb_app.py
@@ -0,0 +1,99 @@
+import streamlit as st
+import cv2
+import numpy as np
+
+st.set_page_config(
+    page_title='인공지능이 보는 세상'
+)
+
+st.title(':computer:인공지능이 보는 세상')
+st.divider()
+
+col1, col2, col3 = st.columns([1, 2, 1])
+with col2:
+    img_file_buffer = st.camera_input('웹캠 데이터')
+
+
+if img_file_buffer is not None:
+    # To read image file buffer with OpenCV:
+    bytes_data = img_file_buffer.getvalue()
+    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+    col1, col2, col3 = st.columns([1, 2, 1])
+    with col2:
+        st.write('이미지의 픽셀:', cv2_img.shape)
+
+    # RGB로 변환
+    rgb_frame = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
+
+    # 각 채널로 분리
+    r_channel, g_channel, b_channel = cv2.split(rgb_frame)
+    
+    # 색의 합성
+    merged_r = cv2.merge([r_channel, np.zeros_like(r_channel), np.zeros_like(r_channel)])
+    merged_g = cv2.merge([np.zeros_like(g_channel) , g_channel, np.zeros_like(g_channel)])
+    merged_b = cv2.merge([np.zeros_like(b_channel), np.zeros_like(r_channel), b_channel])
+    merged_rgb = cv2.merge([r_channel, g_channel, b_channel])
+    merged_rg = cv2.merge([r_channel, g_channel, np.zeros_like(b_channel)])
+    merged_gb = cv2.merge([np.zeros_like(r_channel), g_channel, b_channel])
+    merged_rb = cv2.merge([r_channel, np.zeros_like(g_channel), b_channel])
+    
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.write('Red')
+        st.image(r_channel)
+        st.write(r_channel.shape)
+
+    with col2:
+        st.write('Green')
+        st.image(g_channel)
+        st.write(g_channel.shape)
+
+    with col3:
+        st.write('Blue')
+        st.image(b_channel)
+        st.write(b_channel.shape)
+    st.divider()
+    
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.write('Red')
+        st.write(r_channel)
+
+    with col2:
+        st.write('Green')
+        st.write(g_channel)
+
+    with col3:
+        st.write('Blue')
+        st.write(b_channel)
+    st.divider()
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.write('Red')
+        st.image(merged_r)
+
+    with col2:
+        st.write('Green')
+        st.image(merged_g)
+
+    with col3:
+        st.write('Blue')
+        st.image(merged_b)
+    st.divider()
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.write('R+G')
+        st.image(merged_rg)
+
+    with col2:
+        st.write('G+B')
+        st.image(merged_gb)
+
+    with col3:
+        st.write('R + B')
+        st.image(merged_rb)
+    st.divider()
+    col1, col2, col3 = st.columns(3)
+    with col2:
+        st.write('R + G + B')
+        st.image(merged_rgb)
